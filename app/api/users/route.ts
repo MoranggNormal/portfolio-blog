1b04c09cbc1f1379dc44e8629884d9a61c622987
Euller Peixoto <epeixoto.dev@gmail.com>
2023-11-26 09:01:14 -0300

    setup prisma with supabase

---
+++ app/api/users/route.ts
@@ -0,0 +1,25 @@
+import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const users = await prisma.users.findMany({
+    select: {
+      name: true,
+      email: true
+    }
+  });
+
+  return NextResponse.json(users);
+}
+export async function POST(request: Request) {
+  const json = await request.json();
+
+  const user = await prisma.users.create({
+    data: json,
+  });
+
+  return new NextResponse(JSON.stringify(user), {
+    status: 201,
+    headers: { "Content-Type": "application/json" },
+  });
+}
---
+++ app/blog/page.tsx
@@ -0,0 +1,21 @@
+import React, { cache } from "react";
+import { users } from "@prisma/client";
+
+const getUsers = cache(() =>
+  fetch("http://localhost:3000/api/users").then((res) => res.json())
+);
+
+const Blog = () => {
+
+  let users = React.use<users[]>(getUsers());
+
+  return (
+    <div>
+      {users && users.map(user => (
+        user.name
+      ))}
+    </div>
+  )
+}
+
+export default Blog
