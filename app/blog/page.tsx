f9aba8cc9823028cd6d6d752a028cb2c0b26a0d5
Euller Peixoto <epeixoto.dev@gmail.com>
2023-11-26 09:24:14 -0300

    Update page.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,19 +1,20 @@
-import React, { cache } from "react";
-import { users } from "@prisma/client";
+// import React, { cache } from "react";
+// import { users } from "@prisma/client";
 
-const getUsers = cache(() =>
-  fetch("http://localhost:3000/api/users").then((res) => res.json())
-);
+// const getUsers = cache(() =>
+  // fetch("http://localhost:3000/api/users").then((res) => res.json())
+// );
 
 const Blog = () => {
 
-  let users = React.use<users[]>(getUsers());
+  // let users = React.use<users[]>(getUsers());
 
   return (
     <div>
-      {users && users.map(user => (
+      blog-desu
+      {/* {users && users.map(user => (
         user.name
-      ))}
+      ))} */}
     </div>
   )
 }
