34ba02387dc0d2935f116a114a9cb92e6bd339bd
Euller Peixoto <epeixoto.dev@gmail.com>
2024-01-15 20:15:19 -0300

    update blog button

--- components/portfolio/LatestBlogPosts.tsx
+++ components/portfolio/LatestBlogPosts.tsx
@@ -14,7 +14,7 @@ const LatestBlogPosts = () => {
               href="#"
               className="rounded px-4 py-2 font-semibold  text-slate-800 ring-1 ring-gray-900/10 transition-colors duration-300 ease-in-out hover:bg-slate-800 hover:text-white hover:ring-gray-900/20 hover:drop-shadow-3xl"
             >
-              View Blog
+              Currently being built
             </a>
           </div>
         </div>
